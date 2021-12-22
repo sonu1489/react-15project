54db8c8644f6fe9a212ebc02a8b38d04df676e35
sonu1489 <sharmasonu1489@gmail.com>
2021-12-22 15:38:20 +0530

    react-15-project

---
+++ 12-sidebar-modal/setup/src/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Home = () => {
+  const { openModal, openSidebar } = useGlobalContext();
+
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium
+        accusantium, voluptate doloremque nihil dolorem, repellendus numquam,
+        eveniet cumque reiciendis et natus excepturi. Perferendis velit itaque
+        error quod quaerat soluta sed.
+      </p>
+    </section>
+  );
+};
+
+export default About;
